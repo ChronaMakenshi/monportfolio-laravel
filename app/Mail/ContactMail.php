1dc8bc7eac9fe1a3b7305b3e375d80ff639341f1
Mestdagh christophe <77621850+ChronaMakenshi@users.noreply.github.com>
2023-07-31 11:06:01 +0200

    email

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -38,7 +38,7 @@ class ContactMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            markdown: 'emails.contact',
+            html: 'emails.contact',
         );
     }
 
